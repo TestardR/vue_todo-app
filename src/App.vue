69630a8a85f75c9ff524b06ac1a837ebcccddb5a
TestardR <romain.rtestard@gmail.com>
2021-04-22 17:42:17 +0200

    I have implemented CRUD

--- src/App.vue
+++ src/App.vue
@@ -35,42 +35,75 @@ export default {
       showAddTask: false,
     };
   },
-  created() {
-    this.tasks = [
-      {
-        id: "1",
-        text: "Doctors Appointment",
-        day: "March 5th at 2:30pm",
-        reminder: true,
-      },
-      {
-        id: "2",
-        text: "Meeting with boss",
-        day: "March 6th at 1:30pm",
-        reminder: true,
-      },
-      {
-        id: "3",
-        text: "Food shopping",
-        day: "March 7th at 2:00pm",
-        reminder: false,
-      },
-    ];
+  async created() {
+    this.tasks = await this.fetchTasks();
   },
   methods: {
-    addTask(task) {
-      this.tasks = [...this.tasks, task];
+    async addTask(task) {
+      try {
+        const res = await fetch("api/tasks", {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(task),
+        });
+        const data = await res.json();
+        this.tasks = [...this.tasks, data];
+      } catch (error) {
+        console.log(error.message);
+      }
     },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
+    async deleteTask(id) {
+      try {
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async fetchTasks() {
+      try {
+        const res = await fetch("api/tasks");
+        return await res.json();
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async fetchTask(id) {
+      try {
+        const res = await fetch(`api/tasks/${id}`);
+        return await res.json();
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async toggleReminder(id) {
+      try {
+        const taskToToggle = await this.fetchTask(id);
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(updTask),
+        });
+
+        const data = await res.json();
+
+        this.tasks = this.tasks.map((task) =>
+          task.id === id ? { ...task, reminder: data.reminder } : task
+        );
+      } catch (error) {
+        console.log(error.message);
+      }
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
-    },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      );
     },
   },
 };
